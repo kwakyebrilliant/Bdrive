af2f18a45419c0539e53dad0a345cd2d6aa2b6b8
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-02-27 18:44:09 +0000

    package updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
 
 function App() {
+
   return (
-    <>
-     
-    </>
+    <Router>
+      <Routes>
+      <Route path="/" exact element={<HomePage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navigation from './Navigation'
+
+export default function HomePage() {
+  return (
+    <div>
+        <Navigation />
+        HomePage
+    </div>
+  )
+}
