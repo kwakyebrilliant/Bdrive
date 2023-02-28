aa3285483670b58380aa9e0dfceaba59c1e7f639
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-02-28 22:50:50 +0000

    homepage updates

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -8,20 +8,17 @@ export default function HomePage() {
     <div>
         <Navigation />
 
-        <div className='w-full'>
-            <div className='max-w-[1240px] mx-auto'>
-
-            <div className='text-center pt-40'>
-                <h1 className='text-5xl font-bold'>Secure All Your Contents On The Block</h1>
-                <p className=' text-xl py-6 text-gray-500'>
-                Store, share, and collaborate on files and folders from your mobile device, tablet, or computer
-                </p>
-
-                <div className="flex flex-wrap justify-center gap-4 mt-8 text-center">
-                <a class="group relative inline-flex items-center overflow-hidden rounded bg-blue-700 px-8 py-3 text-white focus:outline-none focus:ring active:bg-blue-700" href="/">
-                <span class="absolute left-0 -translate-x-full transition-transform group-hover:translate-x-4">
+        <div className='w-full h-screen px-20 bg-white flex flex-col justify-between'>
+        <div className='grid md:grid-cols-2 max-w-[1240px] m-auto'>
+            <div className='flex flex-col justify-center md:items-start w-full px-2 py-8'>
+                <h1 className='py-3 text-4xl md:text-6xl font-bold'>Secure <strong className="font-extrabold text-blue-700 sm:block">Contents</strong></h1>
+                <p className='max-w-lg sm:leading-relaxed sm:text-xl'>Store, share, and collaborate on files </p>
+                <p className='max-w-lg sm:leading-relaxed sm:text-xl'>with everything running on the blockchain.</p>
+                <div className="flex flex-wrap gap-4 mt-8 text-center">
+                <a className="group relative inline-flex items-center overflow-hidden rounded bg-blue-700 px-8 py-3 text-white focus:outline-none focus:ring active:bg-blue-700" href="/">
+                <span className="absolute left-0 -translate-x-full transition-transform group-hover:translate-x-4">
                     <svg
-                    class="h-5 w-5"
+                    className="h-5 w-5"
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
@@ -36,26 +33,323 @@ export default function HomePage() {
                     </svg>
                 </span>
 
-                <span class="text-sm font-medium transition-all group-hover:ml-4">
+                <span className="text-sm font-medium transition-all group-hover:ml-4">
                     Get Started
                 </span>
                 </a>
 
-                <a class="group relative inline-block overflow-hidden border border-blue-700 px-8 py-3 focus:outline-none focus:ring" href="/">
-                <span class="absolute inset-x-0 top-0 h-[2px] bg-blue-700 transition-all group-hover:h-full group-active:bg-blue-700"></span>
+                <a className="group relative inline-block overflow-hidden border border-blue-700 px-8 py-3 focus:outline-none focus:ring" href="/">
+                <span className="absolute inset-x-0 top-0 h-[2px] bg-blue-700 transition-all group-hover:h-full group-active:bg-blue-700"></span>
 
-                <span class="relative text-sm font-medium text-blue-700 transition-colors group-hover:text-white">
+                <span className="relative text-sm font-medium text-blue-700 transition-colors group-hover:text-white">
                     Learn More
                 </span>
                 </a>
                 </div>
-                
-            </div>
 
+            </div>
             
-
+            <div className='mx-auto max-w-lg text-center lg:mx-0 lg:text-left'>
+                <img className='w-full justify-center' src={bg} alt="/" />
             </div>
+           
+
+           
         </div>
+
+        <div className="relative ">
+        <div className="absolute inset-0 flex items-center">
+            <div className="w-full border-b border-gray-300"></div>
+        </div>
+        </div>
+
+    </div>
+
+
+
+    <section>
+  <div className="max-w-screen-xl px-4 py-8 sm:py-12 sm:px-6 lg:py-16 lg:px-8">
+    <div
+      className="grid grid-cols-1 gap-y-8 lg:grid-cols-2 lg:items-center lg:gap-x-16"
+    >
+      <div className="mx-auto max-w-lg text-center lg:mx-0 lg:text-left">
+        <img className='w-full justify-center' src={bg} alt="/" />
+      </div>
+
+      <div className="grid grid-cols-2 gap-4 sm:grid-cols-3">
+
+        <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring" href="/accountant">
+          <span className="inline-block rounded-lg bg-gray-50 p-3">
+            <svg
+              className="h-6 w-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+              <path
+                d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+              ></path>
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+              ></path>
+            </svg>
+          </span>
+
+          <h2 className="mt-2 font-bold">Accountant</h2>
+
+          <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
+            Lorem ipsum dolor sit amet consectetur.
+          </p>
+        </a>
+
+        <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring" href="/accountant">
+          <span className="inline-block rounded-lg bg-gray-50 p-3">
+            <svg
+              className="h-6 w-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+              <path
+                d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+              ></path>
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+              ></path>
+            </svg>
+          </span>
+
+          <h2 className="mt-2 font-bold">Accountant</h2>
+
+          <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
+            Lorem ipsum dolor sit amet consectetur.
+          </p>
+        </a>
+
+        <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring" href="/accountant">
+          <span className="inline-block rounded-lg bg-gray-50 p-3">
+            <svg
+              className="h-6 w-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+              <path
+                d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+              ></path>
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+              ></path>
+            </svg>
+          </span>
+
+          <h2 className="mt-2 font-bold">Accountant</h2>
+
+          <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
+            Lorem ipsum dolor sit amet consectetur.
+          </p>
+        </a>
+
+        <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring" href="/accountant">
+          <span className="inline-block rounded-lg bg-gray-50 p-3">
+            <svg
+              className="h-6 w-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+              <path
+                d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+              ></path>
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+              ></path>
+            </svg>
+          </span>
+
+          <h2 className="mt-2 font-bold">Accountant</h2>
+
+          <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
+            Lorem ipsum dolor sit amet consectetur.
+          </p>
+        </a>
+
+        <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring" href="/accountant">
+          <span className="inline-block rounded-lg bg-gray-50 p-3">
+            <svg
+              className="h-6 w-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+              <path
+                d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+              ></path>
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+              ></path>
+            </svg>
+          </span>
+
+          <h2 className="mt-2 font-bold">Accountant</h2>
+
+          <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
+            Lorem ipsum dolor sit amet consectetur.
+          </p>
+        </a>
+
+        <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring" href="/accountant">
+          <span className="inline-block rounded-lg bg-gray-50 p-3">
+            <svg
+              className="h-6 w-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+              <path
+                d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+              ></path>
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+              ></path>
+            </svg>
+          </span>
+
+          <h2 className="mt-2 font-bold">Accountant</h2>
+
+          <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
+            Lorem ipsum dolor sit amet consectetur.
+          </p>
+        </a>
+
+        <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring" href="/accountant">
+          <span className="inline-block rounded-lg bg-gray-50 p-3">
+            <svg
+              className="h-6 w-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+              <path
+                d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+              ></path>
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+              ></path>
+            </svg>
+          </span>
+
+          <h2 className="mt-2 font-bold">Accountant</h2>
+
+          <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
+            Lorem ipsum dolor sit amet consectetur.
+          </p>
+        </a>
+
+        <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring" href="/accountant">
+          <span className="inline-block rounded-lg bg-gray-50 p-3">
+            <svg
+              className="h-6 w-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+              <path
+                d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+              ></path>
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+              ></path>
+            </svg>
+          </span>
+
+          <h2 className="mt-2 font-bold">Accountant</h2>
+
+          <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
+            Lorem ipsum dolor sit amet consectetur.
+          </p>
+        </a>
+
+        <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring" href="/accountant">
+          <span className="inline-block rounded-lg bg-gray-50 p-3">
+            <svg
+              className="h-6 w-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+              <path
+                d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+              ></path>
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+              ></path>
+            </svg>
+          </span>
+
+          <h2 className="mt-2 font-bold">Accountant</h2>
+
+          <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
+            Lorem ipsum dolor sit amet consectetur.
+          </p>
+        </a>
+      
+
+       
+
+       
+      </div>
+    </div>
+  </div>
+</section>
+
+
+  
         
     </div>
   )
