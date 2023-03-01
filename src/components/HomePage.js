03383ee106b8003f7537d33e73e590f840cc23f4
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-03-01 13:55:24 +0000

    homepage updates

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,6 +3,14 @@ import Navigation from './Navigation'
 
 import bg from '../assets/bg.jpg'
 import img1 from '../assets/img1.jpg'
+import supportImg from '../assets/support.jpeg'
+
+import { Link } from 'react-router-dom'
+import {
+    ChipIcon, 
+    SupportIcon
+  } from '@heroicons/react/solid'
+  import { PhoneIcon, ArrowSmRightIcon } from '@heroicons/react/outline';
 
 export default function HomePage() {
   return (
@@ -350,6 +358,139 @@ export default function HomePage() {
 </section>
 
 
+<div className='w-full mt-24'>
+
+<div className='w-full h-[700px] bg-gray-900/90 absolute'>
+    <img className='w-full h-full object-cover mix-blend-overlay' src={supportImg} alt="/" />
+</div>
+
+<div className='max-w-[1240px] mx-auto text-white relative'>
+    <div className='px-4 py-12'>
+      <h2 className='text-3xl pt-8 text-slate-300 uppercase text-center'>Support</h2>
+      <h3 className='text-5xl font-bold py-6 text-center'>Got Questions?</h3>
+      <p className='py-4 text-3xl text-slate-300 text-center'>
+        We have a reliable team available to assist you in all your questions and help you go about your 
+        activities on the platform. Remember we are here to serve you to get the best of deals available on
+        this platform.
+    </p>
+    </div>
+
+    <div className='grid grid-cols-1 lg:grid-cols-3 relative gap-x-8 gap-y-16 px-4 pt-12 sm:pt-20 text-black'>
+
+    <div className="relative hover:shadow-lg block bg-white p-8 overflow-hidden border border-gray-100 rounded-lg">
+            <span
+                className="absolute inset-x-0 bottom-0 h-2  bg-gradient-to-r from-green-300 via-blue-500 to-purple-600"
+            ></span>
+
+            <div className="justify-between sm:flex">
+                <div>
+                <h5 className="text-2xl font-bold text-gray-900">
+                    Sales
+                </h5>
+                <p className="mt-1 text-xs font-medium text-gray-600">By John Doe</p>
+                </div>
+
+                <div className="flex-shrink-0 hidden ml-3 sm:block">
+                <PhoneIcon className='w-16 h-16 p-4 object-cover bg-green-600 shadow-2xl text-white rounded-lg' />
+                </div>
+            </div>
+
+            <div className="mt-4 sm:pr-8">
+                <p className="text-sm text-gray-500">
+                We have a reliable team on standby ready to provide answers to all questions
+                with regards to how to navigate your way on the platform.
+                </p>
+            </div>
+
+            <dl className="flex mt-6">
+                <div className="flex flex-col-reverse">
+                <Link to="#faqs">
+                <dt className="text-sm font-medium flex text-green-600">Continue <ArrowSmRightIcon className='w-5 ml-2' /></dt>
+                    </Link>
+                <dd className="text-xs text-gray-500">Continue onFAQ's</dd>
+                </div>
+
+            </dl>
+            </div>
+
+            <div className="relative hover:shadow-lg block bg-white p-8 overflow-hidden border border-gray-100 rounded-lg">
+            <span
+                className="absolute inset-x-0 bottom-0 h-2  bg-gradient-to-r from-green-300 via-blue-500 to-purple-600"
+            ></span>
+
+            <div className="justify-between sm:flex">
+                <div>
+                <h5 className="text-2xl font-bold text-gray-900">
+                Technical Support
+                </h5>
+                <p className="mt-1 text-xs font-medium text-gray-600">By John Doe</p>
+                </div>
+
+                <div className="flex-shrink-0 hidden ml-3 sm:block">
+                <SupportIcon className='w-16 h-16 p-4 object-cover bg-green-600 shadow-2xl text-white rounded-lg' />
+                </div>
+            </div>
+
+            <div className="mt-4 sm:pr-8">
+                <p className="text-sm text-gray-500">
+                We have a reliable team on standby ready to assist you on how
+                to use the platform to acquire a secure contents and use them at any time.
+                </p>
+            </div>
+
+            <dl className="flex mt-6">
+                <div className="flex flex-col-reverse">
+                <Link to="#faqs">
+                <dt className="text-sm font-medium flex text-green-600">Continue <ArrowSmRightIcon className='w-5 ml-2' /></dt>
+                    </Link>
+                <dd className="text-xs text-gray-500">Continue onFAQ's</dd>
+                </div>
+
+            </dl>
+            </div>
+
+            <div className="relative hover:shadow-lg block bg-white p-8 overflow-hidden border border-gray-100 rounded-lg">
+            <span
+                className="absolute inset-x-0 bottom-0 h-2  bg-gradient-to-r from-green-300 via-blue-500 to-purple-600"
+            ></span>
+
+            <div className="justify-between sm:flex">
+                <div>
+                <h5 className="text-2xl font-bold text-gray-900">
+                    Media Inquiries
+                </h5>
+                <p className="mt-1 text-xs font-medium text-gray-600">By John Doe</p>
+                </div>
+
+                <div className="flex-shrink-0 hidden ml-3 sm:block">
+                <ChipIcon className='w-16 h-16 p-4 object-cover bg-green-600 shadow-2xl text-white rounded-lg' />
+                </div>
+            </div>
+
+            <div className="mt-4 sm:pr-8">
+                <p className="text-sm text-gray-500">
+                We have a reliable team on standby ready to provide you with more
+                media regarding your content on the platform. 
+                </p>
+            </div>
+
+            <dl className="flex mt-6">
+                <div className="flex flex-col-reverse">
+                <Link to="#faqs">
+                <dt className="text-sm font-medium flex text-green-600">Continue <ArrowSmRightIcon className='w-5 ml-2' /></dt>
+                    </Link>
+                <dd className="text-xs text-gray-500">Continue onFAQ's</dd>
+                </div>
+
+            </dl>
+            </div>
+
+            
+    </div>    
+
+</div>
+
+</div>
   
         
     </div>
