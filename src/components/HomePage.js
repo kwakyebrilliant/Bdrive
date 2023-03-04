3500ed67da8d5817f8091685ad6c8f50aee8940e
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-03-04 20:06:26 +0000

    home updates

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -15,7 +15,8 @@ import {
     ArrowSmRightIcon, 
     MicrophoneIcon,
     VideoCameraIcon,
-    DocumentIcon
+    DocumentIcon,
+    PhotographIcon
 } from '@heroicons/react/outline';
 
 export default function HomePage() {
@@ -117,38 +118,21 @@ export default function HomePage() {
           </p>
         </a>
 
-        <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-blue-300 hover:ring-1 hover:ring-blue-300 focus:outline-none focus:ring" href="/accountant">
+        <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-blue-300 hover:ring-1 hover:ring-blue-300 focus:outline-none focus:ring" href="#nothing">
           <span className="inline-block rounded-lg bg-gray-50 p-3">
-          <DocumentIcon className='h-6 w-6' />
+          <PhotographIcon className='h-6 w-6' />
           </span>
 
-          <h2 className="mt-2 font-bold">PDF</h2>
+          <h2 className="mt-2 font-bold">Photo</h2>
 
           <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
             Lorem ipsum dolor sit amet consectetur.
           </p>
         </a>
 
-        <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring" href="/accountant">
+        <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-green-300 hover:ring-1 hover:ring-green-300 focus:outline-none focus:ring" href="#nothing">
           <span className="inline-block rounded-lg bg-gray-50 p-3">
-            <svg
-              className="h-6 w-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-              <path
-                d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-              ></path>
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-              ></path>
-            </svg>
+            <DocumentIcon className='h-6 w-6' />
           </span>
 
           <h2 className="mt-2 font-bold">Word</h2>
@@ -158,26 +142,9 @@ export default function HomePage() {
           </p>
         </a>
 
-        <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring" href="/accountant">
+        <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-green-300 hover:ring-1 hover:ring-green-300 focus:outline-none focus:ring" href="#noting">
           <span className="inline-block rounded-lg bg-gray-50 p-3">
-            <svg
-              className="h-6 w-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-              <path
-                d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-              ></path>
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-              ></path>
-            </svg>
+          <DocumentIcon className='h-6 w-6' />
           </span>
 
           <h2 className="mt-2 font-bold">PPTX</h2>
@@ -187,29 +154,12 @@ export default function HomePage() {
           </p>
         </a>
 
-        <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring" href="/accountant">
+        <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-green-300 hover:ring-1 hover:ring-green-300 focus:outline-none focus:ring" href="#nothing">
           <span className="inline-block rounded-lg bg-gray-50 p-3">
-            <svg
-              className="h-6 w-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-              <path
-                d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-              ></path>
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-              ></path>
-            </svg>
+          <DocumentIcon className='h-6 w-6' />
           </span>
 
-          <h2 className="mt-2 font-bold">Accountant</h2>
+          <h2 className="mt-2 font-bold">PDF</h2>
 
           <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
             Lorem ipsum dolor sit amet consectetur.
