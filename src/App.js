efc6e0d928ca602a082875af2c7a5c7724aec1af
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-03-12 23:23:54 +0000

    app updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import Summary from "./dashboard/Pages/Summary";
+import MyDrive from "./dashboard/Pages/MyDrive";
+import Shared from "./dashboard/Pages/Shared";
+import Favorite from "./dashboard/Pages/Favorite";
+import Recent from "./dashboard/Pages/Recent";
 
 function App() {
 
@@ -9,6 +13,10 @@ function App() {
       <Routes>
       <Route path="/" exact element={<HomePage />} />
       <Route path="/summary" exact element={<Summary />} />
+      <Route path="/mydrive" exact element={<MyDrive />} />
+      <Route path="/shared" exact element={<Shared />} />
+      <Route path="/favorite" exact element={<Favorite />} />
+      <Route path="/Recent" exact element={<Recent />} />
       </Routes>
     </Router>
   );
