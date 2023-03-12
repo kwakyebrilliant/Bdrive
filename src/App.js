8a18e8817ee4577ff6e13e7e4da71794983e69fe
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-03-12 16:58:43 +0000

    app updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
+import Summary from "./dashboard/Pages/Summary";
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <Router>
       <Routes>
       <Route path="/" exact element={<HomePage />} />
+      <Route path="/summary" exact element={<Summary />} />
       </Routes>
     </Router>
   );
