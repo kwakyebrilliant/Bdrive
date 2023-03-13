a70eb03ff13919bf02541637776469af1d028828
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-03-13 19:52:43 +0000

    my drive updates

--- src/dashboard/Pages/MyDrive.js
+++ src/dashboard/Pages/MyDrive.js
@@ -186,13 +186,63 @@ function MyDrive() {
                   />
 
                   <div class="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4">
-                    <strong class="font-medium">Company Name</strong>
+                    <strong class="font-medium">Stands</strong>
 
                     <span class="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
 
-                    <p class="mt-0.5 opacity-50 sm:mt-0">Branding / Signage</p>
+                    <p class="mt-0.5 opacity-50 sm:mt-0">PNG / 2 minutes ago</p>
                   </div>
                 </a>
+
+                <a href="#nothing" class="block">
+                  <img
+                    alt="Signage"
+                    src="https://images.unsplash.com/photo-1588515724527-074a7a56616c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"
+                    class="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-64 lg:h-72"
+                  />
+
+                  <div class="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4">
+                    <strong class="font-medium">Stands</strong>
+
+                    <span class="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
+
+                    <p class="mt-0.5 opacity-50 sm:mt-0">PNG / 2 minutes ago</p>
+                  </div>
+                </a>
+
+                <a href="#nothing" class="block">
+                  <img
+                    alt="Signage"
+                    src="https://images.unsplash.com/photo-1588515724527-074a7a56616c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"
+                    class="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-64 lg:h-72"
+                  />
+
+                  <div class="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4">
+                    <strong class="font-medium">Stands</strong>
+
+                    <span class="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
+
+                    <p class="mt-0.5 opacity-50 sm:mt-0">PNG / 2 minutes ago</p>
+                  </div>
+                </a>
+
+                <a href="#nothing" class="block">
+                  <img
+                    alt="Signage"
+                    src="https://images.unsplash.com/photo-1588515724527-074a7a56616c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"
+                    class="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-64 lg:h-72"
+                  />
+
+                  <div class="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4">
+                    <strong class="font-medium">Stands</strong>
+
+                    <span class="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
+
+                    <p class="mt-0.5 opacity-50 sm:mt-0">PNG / 2 minutes ago</p>
+                  </div>
+                </a>
+
+                
 
 
                 </div>
