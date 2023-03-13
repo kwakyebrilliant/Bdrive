b8dc0b5e83498008889ec248ccc7d680bfcb111f
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-03-13 09:44:28 +0000

    mydrive updates

--- src/dashboard/Pages/MyDrive.js
+++ src/dashboard/Pages/MyDrive.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import PartialNavbar from "../Partials/PartialNavbar";
+import Sidebar from '../Partials/Sidebar';
 
 function MyDrive() {
   return (
-    <div>MyDrive</div>
+    <div className='text-black'>
+        <>
+        <div className='flex flex-auto'>
+            <Sidebar />
+            <div className='grow'>
+                <PartialNavbar />
+                
+                My Drive
+
+            </div>
+
+        </div>
+        </>
+    </div>
   )
 }
 
