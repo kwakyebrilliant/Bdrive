3a05d9f55e214043b7915b0955e4e1cb93c271d5
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-03-15 10:38:43 +0000

    summary updates

--- src/dashboard/Pages/Summary.js
+++ src/dashboard/Pages/Summary.js
@@ -34,6 +34,88 @@ function Summary() {
                 
                 </div>
 
+
+                {showModal ? (
+                                <>
+                                <div
+                                    className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+                                >
+                                    <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                                    {/*content*/}
+                                    <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                                        {/*header*/}
+                                        <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                                        <h3 className="text-xl font-semibold">
+                                            Share File
+                                        </h3>
+                                        <button
+                                            className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                            onClick={() => setShowModal(false)}
+                                        >
+                                            <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                                            ×
+                                            </span>
+                                        </button>
+                                        </div>
+                                        {/*body*/}
+                                        <div className="relative space-y-4 p-6 flex-auto">
+                                       
+
+                                    <p className='text-md font-medium'>
+                                        Enter Receiver's Address
+                                    </p>
+
+                                    <div>
+                                        <label className="sr-only" for="address">Address</label>
+                                        <input
+                                        className="w-96 rounded-lg border-gray-200 shadow-md p-3 text-sm"
+                                        placeholder="eg. Polygon Address"
+                                        type="text"
+                                        id="address"
+                                        />
+                                    </div>
+
+                                    <a className="group w-full relative inline-flex items-center overflow-hidden rounded bg-blue-600 px-8 py-3 text-white focus:outline-none focus:ring active:bg-green-600" href="/">
+                                        <span className="absolute left-0 -translate-x-full transition-transform group-hover:translate-x-4">
+                                            <svg
+                                            className="h-5 w-5"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            stroke="currentColor"
+                                            >
+                                            <path
+                                                stroke-linecap="round"
+                                                stroke-linejoin="round"
+                                                stroke-width="2"
+                                                d="M17 8l4 4m0 0l-4 4m4-4H3"
+                                            />
+                                            </svg>
+                                        </span>
+
+                                        <span className="text-sm font-medium transition-all group-hover:ml-4">
+                                            Submit
+                                        </span>
+                                        </a>
+
+                                        </div>
+                                        {/*footer*/}
+                                        <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                                        <button
+                                            className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                            type="button"
+                                            onClick={() => setShowModal(false)}
+                                        >
+                                            Close
+                                        </button>
+                                        </div>
+                                    </div>
+                                    </div>
+                                </div>
+                                <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                                </>
+                            ) : null}
+
                 <div className='w-full'>
 
                 <div className='p-8'>
