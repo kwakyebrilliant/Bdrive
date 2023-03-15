a25c403d9e36c428d35e6b61a8497679fcc7a9da
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-03-15 22:01:27 +0000

    summary updates

--- src/dashboard/Pages/Summary.js
+++ src/dashboard/Pages/Summary.js
@@ -79,7 +79,15 @@ function Summary() {
                                         Choose File
                                     </p> 
 
-                                    
+                                    <div class="flex w-full items-center justify-center bg-grey-lighter">
+    <label class="w-64 flex flex-col items-center px-4 py-6 bg-white text-blue rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer hover:bg-blue-600 hover:text-white">
+        <svg class="w-8 h-8" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+            <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
+        </svg>
+        <span class="mt-2 text-base leading-normal">Select a file</span>
+        <input type='file' class="hidden" />
+    </label>
+</div>
 
                                     {/* <div className="flex items-center justify-center w-full">
                                         <label for="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-blue-600 dark:bg-gray-50 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-blue-600">
