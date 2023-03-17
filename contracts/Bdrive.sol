4039b72e54b58fe1069a92e103978e9812cc8976
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-03-17 09:09:03 +0000

    bdrive sc updates

--- contracts/Bdrive.sol
+++ contracts/Bdrive.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "hardhat/console.sol";
 
-contract Lock {
+contract Bdrive is ERC721URIStorage {
+
 
 }
