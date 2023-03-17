1451c2407a499e7bdb2077cc020919bb0352bc5a
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-03-17 09:57:10 +0000

    Bdrive sc updates

--- contracts/Bdrive.sol
+++ contracts/Bdrive.sol
@@ -1,11 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "hardhat/console.sol";
-
 contract Bdrive {
 
 
