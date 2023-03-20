cb8f0204aae868c5e158cd1c785277f655a8d2e9
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-03-20 11:53:32 +0000

    summary updates

--- src/dashboard/Pages/Summary.js
+++ src/dashboard/Pages/Summary.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useRef, useEffect} from 'react'
 import PartialNavbar from "../Partials/PartialNavbar";
 import Sidebar from '../Partials/Sidebar';
 
