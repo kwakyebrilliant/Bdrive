0bed4f0becdc18d49b2488cd881e2ee2c2062d43
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-03-21 09:40:50 +0000

    summary updates

--- src/dashboard/Pages/Summary.js
+++ src/dashboard/Pages/Summary.js
@@ -62,6 +62,17 @@ function Summary() {
       return cid
   
     };
+
+    var myDate = new Date();
+    var hours= myDate.getHours();
+    var greet;
+  
+    if (hours < 12)
+        greet =  "morning";
+    else if (hours >= 12 && hours <= 17)
+        greet = "afternoon";
+    else if (hours >= 17 && hours <= 24)
+        greet = "evening";
   
   
     const [haveMetamask, sethaveMetamask] = useState(true);
