c92d3b7b206de30dd81e242bb2ede05f0f3c0f25
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-03-24 10:01:44 +0000

    summary updates

--- src/dashboard/Pages/Summary.js
+++ src/dashboard/Pages/Summary.js
@@ -79,7 +79,22 @@ function Summary() {
       };
 
 
-      
+      async function uploadFile() {
+        if (!name) return;
+        if (!image) return;
+
+        // If MetaMask exists
+    if (typeof window.ethereum !== "undefined") {
+    // await requestAccount();
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+
+    
+
+  }
+
+      }
     
   return (
     <div className='text-black'>
