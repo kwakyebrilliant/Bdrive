cd80583417a237805c8cb5d6363019e438e9f2ce
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-03-25 17:11:33 +0000

    shared updates

--- src/dashboard/Pages/Shared.js
+++ src/dashboard/Pages/Shared.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import PartialNavbar from "../Partials/PartialNavbar";
 import Sidebar from '../Partials/Sidebar';
 
-import { FaShare } from 'react-icons/fa'
 
 import { ethers } from "ethers";
 import BDrive from "../../artifacts/contracts/Bdrive.sol/Bdrive.json";
@@ -12,7 +11,7 @@ const BDRIVE_ABI = BDrive.abi;
 
 
 function Shared() {
-  const [showModal, setShowModal] = useState(false);
+  const [search, setSearch] = useState("");
 
   const [provider, setProvider] = useState(null);
   const [contract, setContract] = useState(null);
@@ -76,7 +75,7 @@ function Shared() {
                         <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                             <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                         </div>
-                        <input type="search" id="search" className="block w-full p-4 pl-10 text-sm border border-gray-300 rounded-lg bg-blue-200 focus:ring-blue-500 focus:border-blue-500 dark:bg-blue-100 dark:border-gray-600 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required />
+                        <input type="search" onChange={(e) => setSearch(e.target.value)} value={search}  id="search" className="block w-full p-4 pl-10 text-sm border border-gray-300 rounded-lg bg-blue-200 focus:ring-blue-500 focus:border-blue-500 dark:bg-blue-100 dark:border-gray-600 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required />
                         <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-600 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-600 dark:focus:ring-blue-800">Search</button>
                     </div>
                 </form>
@@ -92,7 +91,7 @@ function Shared() {
 
                 <div className='w-full relative mt-3 grid grid-cols-1 lg:grid-cols-4 gap-x-8 gap-y-16'>
                 {sharedFiles.length > 0 ? (
-                  sharedFiles.map((file, i) => (
+                  sharedFiles.filter((file) => file.name.match(new RegExp(search, 'i'))).map((file, i) => (
 
                       <div key={file.name}>
                         <a href='#nothing' className='block'>
