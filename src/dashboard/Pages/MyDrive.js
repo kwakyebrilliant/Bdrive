4121f25391b0c6f905e3d01343f2a870d9f0ec88
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-03-25 16:01:32 +0000

    mydrive updates

--- src/dashboard/Pages/MyDrive.js
+++ src/dashboard/Pages/MyDrive.js
@@ -17,7 +17,11 @@ function MyDrive() {
   const [files, setFiles] = useState([]);
   const [search, setSearch] = useState("");
   
-  
+  const [fileIndex, setFileIndex] = useState("");
+  const [recipientAddress, setRecipientAddress] = useState("");
+  const [statusMessage, setStatusMessage] = useState("");
+  const [isMetamaskInstalled, setIsMetamaskInstalled] = useState(false);
+  const [isMetamaskConnected, setIsMetamaskConnected] = useState(false);
 
   useEffect(() => {
     async function fetchData() {
@@ -39,6 +43,39 @@ function MyDrive() {
     }
     fetchData();
   }, []);
+
+
+  useEffect(() => {
+    async function checkMetamask() {
+      if (typeof window.ethereum !== "undefined") {
+        setIsMetamaskInstalled(true);
+        try {
+          await window.ethereum.request({ method: "eth_requestAccounts" });
+          setIsMetamaskConnected(true);
+        } catch (err) {
+          setIsMetamaskConnected(false);
+        }
+      } else {
+        setIsMetamaskInstalled(false);
+      }
+    }
+    checkMetamask();
+  }, []);
+
+  async function shareFile() {
+    try {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const bdriveAddress = "0x7b06D17d015500968AA413611f763F5e10F17Df2";
+      const contract = new ethers.Contract(bdriveAddress, BDrive.abi, signer);
+
+      await contract.shareFile(fileIndex, recipientAddress);
+      setStatusMessage("File shared successfully!");
+    } catch (err) {
+      console.error(err);
+      setStatusMessage("Error sharing file: " + err.message);
+    }
+  }
 
   // async function getImage(cid) {
   //   const web3Storage = new Web3Storage({ token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDE0ZGU4NTUwMjAxMTdENDIyY0IxOTRBREJiZERlOTJGZjBkYzkxNzciLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NzkzMTA5MjU2NDcsIm5hbWUiOiJCRHJpdmUifQ.hQVswoHltLw7O53wrarZP5lVW00dTI-lW6GmE4ozt6Q" });
@@ -79,85 +116,117 @@ function MyDrive() {
                 </form>
 
                 {showModal ? (
-                                <>
-                                <div
-                                    className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-                                >
-                                    <div className="relative w-auto my-6 mx-auto max-w-3xl">
-                                    {/*content*/}
-                                    <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                                        {/*header*/}
-                                        <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                                        <h3 className="text-xl font-semibold">
-                                            Share File
-                                        </h3>
-                                        <button
-                                            className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                                            onClick={() => setShowModal(false)}
-                                        >
-                                            <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                                            ×
-                                            </span>
-                                        </button>
-                                        </div>
-                                        {/*body*/}
-                                        <div className="relative space-y-4 p-6 flex-auto">
-                                       
+                <div>
+                  {isMetamaskInstalled ? (
+                    isMetamaskConnected ? (
+                      <>
+                      <div
+                          className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+                      >
+                          <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                          {/*content*/}
+                          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                              {/*header*/}
+                              <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                              <h3 className="text-xl font-semibold">
+                                  Share File
+                              </h3>
+                              <button
+                                  className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                  onClick={() => setShowModal(false)}
+                              >
+                                  <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                                  ×
+                                  </span>
+                              </button>
+                              </div>
+                              {/*body*/}
+                              <div className="relative space-y-4 p-6 flex-auto">
+                             
+                              <p className='text-md font-medium'>
+                              Enter File Index
+                          </p>
 
-                                    <p className='text-md font-medium'>
-                                        Enter Receiver's Address
-                                    </p>
+                          <div>
+                              <label className="sr-only" for="index">Number</label>
+                              <input
+                              className="w-96 rounded-lg border-gray-200 shadow-md p-3 text-sm"
+                              placeholder="eg. 1"
+                              type="number"
+                              id="index"
+                              value={fileIndex} 
+                              onChange={(e) => setFileIndex(e.target.value)}
+                              />
+                          </div>
 
-                                    <div>
-                                        <label className="sr-only" for="address">Address</label>
-                                        <input
-                                        className="w-96 rounded-lg border-gray-200 shadow-md p-3 text-sm"
-                                        placeholder="eg. Polygon Address"
-                                        type="text"
-                                        id="address"
-                                        />
-                                    </div>
+                          <p className='text-md font-medium'>
+                              Enter Receiver's Address
+                          </p>
 
-                                    <a className="group w-full relative inline-flex items-center overflow-hidden rounded bg-blue-600 px-8 py-3 text-white focus:outline-none focus:ring active:bg-green-600" href="/">
-                                        <span className="absolute left-0 -translate-x-full transition-transform group-hover:translate-x-4">
-                                            <svg
-                                            className="h-5 w-5"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            fill="none"
-                                            viewBox="0 0 24 24"
-                                            stroke="currentColor"
-                                            >
-                                            <path
-                                                stroke-linecap="round"
-                                                stroke-linejoin="round"
-                                                stroke-width="2"
-                                                d="M17 8l4 4m0 0l-4 4m4-4H3"
-                                            />
-                                            </svg>
-                                        </span>
+                          <div>
+                              <label className="sr-only" for="address">Address</label>
+                              <input
+                              className="w-96 rounded-lg border-gray-200 shadow-md p-3 text-sm"
+                              placeholder="eg. Polygon Address"
+                              type="text"
+                              id="address"
+                              value={recipientAddress} 
+                              onChange={(e) => setRecipientAddress(e.target.value)}
+                              />
+                          </div>
 
-                                        <span className="text-sm font-medium transition-all group-hover:ml-4">
-                                            Submit
-                                        </span>
-                                        </a>
+                          <a onClick={shareFile} className="group w-full relative inline-flex items-center overflow-hidden rounded bg-blue-600 px-8 py-3 text-white focus:outline-none focus:ring active:bg-green-600">
+                              <span className="absolute left-0 -translate-x-full transition-transform group-hover:translate-x-4">
+                                  <svg
+                                  className="h-5 w-5"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  fill="none"
+                                  viewBox="0 0 24 24"
+                                  stroke="currentColor"
+                                  >
+                                  <path
+                                      stroke-linecap="round"
+                                      stroke-linejoin="round"
+                                      stroke-width="2"
+                                      d="M17 8l4 4m0 0l-4 4m4-4H3"
+                                  />
+                                  </svg>
+                              </span>
 
-                                        </div>
-                                        {/*footer*/}
-                                        <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-                                        <button
-                                            className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                            type="button"
-                                            onClick={() => setShowModal(false)}
-                                        >
-                                            Close
-                                        </button>
-                                        </div>
-                                    </div>
-                                    </div>
-                                </div>
-                                <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-                                </>
-                            ) : null}
+                              <span className="text-sm font-medium transition-all group-hover:ml-4">
+                                  Share File
+                              </span>
+                              </a>
+
+                              <div>{statusMessage}</div>
+
+                              </div>
+                              {/*footer*/}
+                              <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                              <button
+                                  className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                  type="button"
+                                  onClick={() => setShowModal(false)}
+                              >
+                                  Close
+                              </button>
+                              </div>
+                          </div>
+                          </div>
+                      </div>
+                      <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                      </>
+                    ) : (
+                      <div>Please connect to MetaMask.</div>
+                    )
+                  ) : (
+                    <div>Please install MetaMask.</div>
+                  )}
+                </div>
+                ): null}
+
+                
+               
 
                 {/* Suggested */}
                 <div className='mt-10'>
