e00b07b1e4b895c2fd053204bd74ca90c93b9433
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-03-25 11:17:42 +0000

    mydrive updates

--- src/dashboard/Pages/MyDrive.js
+++ src/dashboard/Pages/MyDrive.js
@@ -87,36 +87,17 @@ function MyDrive() {
                 </div>
 
             
-                <div className='w-full'>
-
-                  <div className='relative mt-3 grid grid-cols-1 lg:grid-cols-4 gap-x-8 gap-y-16'>
-
-                  <a className="relative block overflow-hidden rounded-xl bg-[url(https://images.unsplash.com/photo-1552832230-c0197dd311b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1592&q=80)] bg-cover bg-center bg-no-repeat"
-                  >
-                    <div className="absolute inset-0 bg-black/25"></div>
-
-                    <div className="relative flex items-start justify-between p-4 sm:p-6 lg:p-8">
-                      <div className="sm:pt-18 pt-12 text-white lg:pt-24">
-                        {/* <h3 className="text-xl font-bold sm:text-2xl">Rome</h3> */}
-                        <span
-                        className="inline-flex items-center gap-0.5 rounded-full bg-black px-2 py-1 text-xs font-semibold text-white"
+                <div className='w-full relative mt-3 grid grid-cols-1 lg:grid-cols-4 gap-x-8 gap-y-16'>
+                  {files.slice(0, 4).map((file) => (
+                    <div key={file.name}>
+                      <iframe src={(file.cid)} alt={file.name}  className="relative border block overflow-hidden rounded-xl bg-cover bg-center bg-no-repeat"
                       >
-                        <p className="text-sm flex justify-between">2 minutes ago</p>
-                        </span>
-                      </div>
-
-
-                      <span
-                        className="inline-flex items-center gap-0.5 rounded-full bg-black px-2 py-1 text-xs font-semibold text-white"
-                      >
-                        <h3 className="text-xl font-bold sm:text-2xl">Rome</h3>
-                      </span>
+                        </iframe>
+                    
                     </div>
-                  </a>
+                  ))
 
-                 
-
-                  </div>
+                  }
 
                 </div>
 
