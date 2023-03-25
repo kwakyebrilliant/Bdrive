60f1d6002106bf0b88415c596d299cba2e7140f7
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-03-25 10:31:55 +0000

    mydrive updates

--- src/dashboard/Pages/MyDrive.js
+++ src/dashboard/Pages/MyDrive.js
@@ -314,154 +314,41 @@ function MyDrive() {
                       <iframe className="h-56 w-full rounded-bl-3xl rounded-tr-3xl border object-cover sm:h-64 lg:h-72" src={(file.cid)} alt={file.name} >
                         </iframe>
                         <div className="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4">
-                          
+
                         <strong className="font-medium">
                           {file.name}
                         </strong>
 
+                        <span className="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
+
+                        <p className="mt-0.5 opacity-50 sm:mt-0">
+                        {new Date(file.timestamp * 1000).toLocaleString()} ago
+                        </p>
+
+                        <span
+                        className="inline-flex items-center gap-0.5 rounded-full bg-black px-2 py-1 text-xs font-semibold text-white"
+                      >
+                        <FaShare className='text-blue-500 cursor-pointer lg:w-4 lg:h-4'  onClick={() => setShowModal(true)} />
+
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-4 w-4 text-red-600"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                        >
+                          <path
+                            d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                          />
+                        </svg>
+                      </span>
+
                         </div>
-                      <p>Timestamp: {new Date(file.timestamp * 1000).toLocaleString()}</p>
                         </a>
                     </div>
                   ))}
                 </div>
 
-                <div className='w-full'>
-                <div className='relative mt-3 grid grid-cols-1 lg:grid-cols-4 gap-x-8 gap-y-16'>
-
-                <a href="#nothing" className="block">
-                  <img
-                    alt="Signage"
-                    src="https://images.unsplash.com/photo-1588515724527-074a7a56616c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"
-                    className="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-64 lg:h-72"
-                  />
-
-                  <div className="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4">
-                    <strong className="font-medium">Stands</strong>
-
-                    <span className="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
-
-                    <p className="mt-0.5 opacity-50 sm:mt-0">2 minutes ago</p>
-                    <span
-                        className="inline-flex items-center gap-0.5 rounded-full bg-black px-2 py-1 text-xs font-semibold text-white"
-                      >
-                        <FaShare className='text-blue-500 cursor-pointer lg:w-4 lg:h-4'  onClick={() => setShowModal(true)} />
-
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="h-4 w-4 text-red-600"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                        >
-                          <path
-                            d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-                          />
-                        </svg>
-                      </span>
-                  </div>
-                </a>
-
-                <a href="#nothing" className="block">
-                  <img
-                    alt="Signage"
-                    src="https://images.unsplash.com/photo-1588515724527-074a7a56616c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"
-                    className="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-64 lg:h-72"
-                  />
-
-                  <div className="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4">
-                    <strong className="font-medium">Stands</strong>
-
-                    <span className="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
-
-                    <p className="mt-0.5 opacity-50 sm:mt-0">2 minutes ago</p>
-                    <span
-                        className="inline-flex items-center gap-0.5 rounded-full bg-black px-2 py-1 text-xs font-semibold text-white"
-                      >
-                        <FaShare className='text-blue-500 cursor-pointer lg:w-4 lg:h-4'  onClick={() => setShowModal(true)} />
-
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="h-4 w-4 text-red-600"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                        >
-                          <path
-                            d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-                          />
-                        </svg>
-                      </span>
-                  </div>
-                </a>
-
-                <a href="#nothing" className="block">
-                  <img
-                    alt="Signage"
-                    src="https://images.unsplash.com/photo-1588515724527-074a7a56616c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"
-                    className="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-64 lg:h-72"
-                  />
-
-                  <div className="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4">
-                    <strong className="font-medium">Stands</strong>
-
-                    <span className="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
-
-                    <p className="mt-0.5 opacity-50 sm:mt-0">2 minutes ago</p>
-                    <span
-                        className="inline-flex items-center gap-0.5 rounded-full bg-black px-2 py-1 text-xs font-semibold text-white"
-                      >
-                        <FaShare className='text-blue-500 cursor-pointer lg:w-4 lg:h-4'  onClick={() => setShowModal(true)} />
-
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="h-4 w-4 text-red-600"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                        >
-                          <path
-                            d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-                          />
-                        </svg>
-                      </span>
-                  </div>
-                </a>
-
-                <a href="#nothing" className="block">
-                  <img
-                    alt="Signage"
-                    src="https://images.unsplash.com/photo-1588515724527-074a7a56616c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"
-                    className="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-64 lg:h-72"
-                  />
-
-                  <div className="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4">
-                    <strong className="font-medium">Stands</strong>
-
-                    <span className="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
-
-                    <p className="mt-0.5 opacity-50 sm:mt-0">2 minutes ago</p>
-                    <span
-                        className="inline-flex items-center gap-0.5 rounded-full bg-black px-2 py-1 text-xs font-semibold text-white"
-                      >
-                        <FaShare className='text-blue-500 cursor-pointer lg:w-4 lg:h-4'  onClick={() => setShowModal(true)} />
-
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="h-4 w-4 text-red-600"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                        >
-                          <path
-                            d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-                          />
-                        </svg>
-                      </span>
-                  </div>
-                </a>
-
                 
-
-
-                </div>
-                </div>
 
 
                 </div>
